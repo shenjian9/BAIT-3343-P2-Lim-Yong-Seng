3057136d5eb1c812df062b9eb3e02c8fcab87f77
taruc <taruc@B80>
2017-11-01 11:01:00 +0800

    my first-branch commit

--- src/f2/F2.java
+++ src/f2/F2.java
@@ -16,6 +16,8 @@ public class F2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("First meassge");
+        System.out.println("First-branch");
     }
     
 }
